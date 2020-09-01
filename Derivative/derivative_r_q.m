7ee6ab83163cec5fc55f864ac9abae13d3dce7b0
Junchen Wang <wangjunchen@buaa.edu.cn>
2020-09-01 18:26:19 +0800

    refine r_q_converter

--- Derivative/derivative_r_q.m
+++ Derivative/derivative_r_q.m
@@ -12,21 +12,5 @@ if a >= 0
     st = sqrt(1 - a^2); % sin(t)
     J = [(2 * t * a - 2 * st) / st^3 * v, 2 * t / st * eye(3)];
 else
-    q = -q;
-    a = q(1);
-    v = q(2 : 4);
-    t = acos(a);
-    st = sqrt(1 - a^2); % sin(t)
-    J = -[(2 * t * a - 2 * st) / st^3 * v, 2 * t / st * eye(3)];
+    J = -derivative_r_q(-q);
 end
-
-% mu = norm(v);
-% tao = 2 * sign(a) * asin(mu) / mu;
-% vv = (2 * a - tao) / mu^2;
-% J = [-2 * v, tao * eye(3) + vv * (v * v')];
-% function f = sign(x)
-% if x >= 0
-%     f = 1;
-% else
-%     f = -1;
-% end
