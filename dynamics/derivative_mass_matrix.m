0b55351e4dde2f14ae2eadd2ef85b0b45b0b229b
Junchen Wang <wangjunchen@buaa.edu.cn>
2022-01-01 23:50:58 +0800

    derivative_mass_matrix OK

--- dynamics/derivative_mass_matrix.m
+++ dynamics/derivative_mass_matrix.m
@@ -9,7 +9,6 @@ dJ = zeros(6, n, n);
 Mq = zeros(n, n);
 dMq = zeros(n, n);
 dA = zeros(size(A));
-% 这个方法计算dJ不对，目前还没找到原因，derivative_mass_matrix2 的结果是对的
 for i = 1 : n
    G = [inertia(:,:,i), zeros(3);zeros(3), mass(i) * eye(3)];
    T =  exp_twist(-A(i,:) * q(i)) * tform_inv(M(:,:,i));
@@ -17,8 +16,8 @@ for i = 1 : n
    J(:, i, i)  = A(i,:)';
    [dAdT, AdT] = derivative_adjoint_T(T, dT);
    for j = 1 : i - 1
-       A(j,:) = AdT * A(j,:)';
        dA(j,:) = dAdT * A(j,:)' + AdT * dA(j, :)';
+       A(j,:) = AdT * A(j,:)';
        J(:, j, i)  = A(j,:)';
        dJ(:, j, i)  = dA(j,:)';
    end
