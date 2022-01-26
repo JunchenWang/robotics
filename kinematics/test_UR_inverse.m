39c1a88c07cfcdffe23cac46d9e3a6df22bbdecd
Junchen@215 <wangjunchen@buaa.edu.cn>
2022-01-26 20:37:12 +0800

    add UR_inverse_kin_near_c to generate code

--- kinematics/test_UR_inverse.m
+++ kinematics/test_UR_inverse.m
@@ -6,9 +6,9 @@ cnt = 1;
 tic;
 for i = 1 : N
     q = rand(1,6);
-    q(3) = deg2rad(10) + rand * deg2rad(120);
-     q(5) = deg2rad(10) + rand * deg2rad(120);
-    [ang, flag] = UR_inverse_kin(robot, UR5e_forward_kin(q), q);
+%     q(3) = deg2rad(10) + rand * deg2rad(120);
+     q(5) = deg2rad(5) + rand * deg2rad(140);
+    [ang, flag] = UR_inverse_kin_c_mex(robot, UR5e_forward_kin(q), q + (rand * 2 - 1) * 0.2);
     if flag == 0
         angles(cnt,:) = q;
         cnt = cnt + 1;
