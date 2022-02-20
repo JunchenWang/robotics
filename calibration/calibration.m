43d83c6ade9448db462cbc1cdfe2b6af2c8e12bc
wangjunchen@317 <wangjunchen@buaa.edu.cn>
2022-02-20 17:18:20 +0800

    add force sensor calibration

---
+++ calibration/calibration.m
@@ -0,0 +1,8 @@
+fid = fopen('force.txt','r');
+force = fscanf(fid, '%f', [6, inf]);
+fclose(fid);
+fid = fopen('pose.txt','r');
+pose = fscanf(fid, '%f', [6, inf]);
+fclose(fid);
+data = [pose', force'];
+[mass, r, offset] = force_sensor_calibration(data);
