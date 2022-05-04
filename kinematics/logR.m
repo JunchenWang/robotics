e4ad4706b83cc39b953f0c1d873f2789a849b435
Junchen@215 <wangjunchen@buaa.edu.cn>
2022-05-04 17:22:31 +0800

    fix logR

--- kinematics/logR.m
+++ kinematics/logR.m
@@ -9,7 +9,7 @@ for i = 1 : n
     if abs(theta - pi) < eps
         w(i,:) = pi / sqrt(2 * (1 + R(1,1,i))) * (R(:,1,i)' + [1 0 0]);
     elseif theta > eps
-        v = [R(3,2,:)-R(2,3,:), R(1,3,:)-R(3,1,:), R(2,1,:)-R(1,2,:)] / (2 * sin(theta));
+        v = [R(3,2,i)-R(2,3,i), R(1,3,i)-R(3,1,i), R(2,1,i)-R(1,2,i)] / (2 * sin(theta));
         normv = norm(v);
         if normv ~= 0
             v = v / normv;
