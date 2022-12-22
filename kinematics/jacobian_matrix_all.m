73f7c72b041aa1c1be8cba4d71c06cf4971910f2
Junchen@home <wangjunchen@buaa.edu.cn>
2022-12-22 21:37:01 +0800

    add jacobian_matrix_all

---
+++ kinematics/jacobian_matrix_all.m
@@ -0,0 +1,13 @@
+function J = jacobian_matrix_all(robot, q)
+A = robot.A;
+M = robot.M;
+n = robot.dof;
+J = zeros(6, n, n);
+for i = n : -1 : 1
+   T = eye(4);
+   for j = i : -1: 1
+       J(:,j,i) = adjoint_T(T) * A(j,:)';
+       T = T * exp_twist(-A(j,:) * q(j)) * tform_inv(M(:,:,j));
+   end
+end
+J(:,:,end) = adjoint_T(InvertT(robot.ME)) * J(:,:,end);
