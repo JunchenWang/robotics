96ebe86501c45aef4a85ed3d0754feb7f442b1d2
Junchen@home <wangjunchen@buaa.edu.cn>
2023-12-12 01:05:06 +0800

    teaching

--- teaching/simulate_joint_pos.m
+++ teaching/simulate_joint_pos.m
@@ -1,21 +1,18 @@
-function simulate_joint
+function simulate_joint_pos
 joint = joint_model(0.1, 0.2, 200);
 y0 = [0, 0, 0]';
 Kp = 1;
-Ki = 2;
-u = @(t, y) controller(t,y, @desired_velocity, Kp, Ki);
+Ki = 10;
+u = @(t, y) controller(t,y, @(t) 5, Kp, Ki);
 d = @(t, y) 10;
 dynamic = @(t, y) joint_dynamics(joint, t, y, u, d);
 tspan = [0, 10];
 [t, y] = ode45(dynamic, tspan, y0);
-plot(t, y(:,2), 'b-', t, desired_velocity(t), 'r-', 'LineWidth',2);
-title('关节速度控制');
+plot(t, y(:,2), 'b-','LineWidth',2);
+title('关节速度控制 d = 10, Kp = 1, Ki = 10');
 xlabel('$t$ / s','interpreter','latex');
-xticks(linspace(0,10, 11));
 ylabel('$\dot{q}$ / (rad/s)', 'interpreter','latex');
 set(gca,'FontSize', 16);
-lg = legend('仿真速度','期望速度');
-fontsize(lg,14,'points')
 end
 
 function [u, qed] = controller(t, y, qd_d, Kp, Ki)
@@ -32,9 +29,4 @@ yd = zeros(3,1);
 yd(1) = y(2);
 yd(2) = (u - d(t, y) / joint.r - joint.B * y(2)) / joint.J;
 yd(3) = e;
-end
-
-function qd_d = desired_velocity(t)
-    % qd_d = 5 * ones(size(t));
-    qd_d = sin(t);
 end
