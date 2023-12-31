17fdca866a74eef8dfca60ac1eaa8ee8f7aa385b
wangjunchen@317 <wangjunchen@buaa.edu.cn>
2023-12-31 19:54:56 +0800

    teaching refine

--- teaching/joint_velocity_control/simulate_joint_open_loop.m
+++ teaching/joint_velocity_control/simulate_joint_open_loop.m
@@ -1,17 +1,28 @@
 function simulate_joint_open_loop
 joint = joint_model(0.1, 0.2, 200);
 y0 = [0, 0]';
-u = @(t) 1;
+u = @(t) open_loop_controller(t, @desired_velocity, joint);
 d = @(t) 10;
 dynamic = @(t, y) joint_dynamics(joint, t, y, u, d);
 tspan = [0, 10];
 [t, y] = ode45(dynamic, tspan, y0);
-plot(t, y(:,2), 'b-', 'LineWidth',2);
-title('关节速度控制——开环');
+qd_d = desired_velocity(t);
+plot(t, y(:,2), 'b', t, qd_d, 'r--', 'LineWidth',2);
+title('关节速度开环控制');
 xlabel('$t$ / s','interpreter','latex');
 xticks(linspace(0,10, 11));
-ylabel('$\dot{q}$ / (rad/s)', 'interpreter','latex');
-set(gca,'FontSize', 16);
+ylabel('$\dot{q}$ / (rad/s)', 'interpreter','latex');    
+yticks(linspace(0,6, 7));
+ylim([0,5.5]);
+set(gca,'FontSize', 32);
+lg = legend('仿真速度','期望速度');
+fontsize(lg,32,'points');
+set(gcf,'Position',[100 100 1200 800]);
+end
+
+function u = open_loop_controller(t, qd_d, joint)
+    [qd_d, qdd_d] = qd_d(t);
+    u = joint.J * qdd_d + joint.B * qd_d;
 end
 
 function yd = joint_dynamics(joint, t, y, u, d)
@@ -19,4 +30,10 @@ function yd = joint_dynamics(joint, t, y, u, d)
 yd = zeros(2,1);
 yd(1) = y(2);
 yd(2) = (u(t) - d(t) / joint.r - joint.B * y(2)) / joint.J;
+end
+
+function [qd_d, qdd_d] = desired_velocity(t)
+    qd_d = 5 * ones(size(t));
+    % qd_d = sin(t);
+    qdd_d = 0;
 end
