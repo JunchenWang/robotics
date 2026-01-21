8e95abe825675e9c8985bb6e3c112c6e45eaa382
wangjunchen <wangjunchen@buaa.edu.cn>
2026-01-21 14:48:13 +0800

    update MICSys-6

--- utilities/query_joints.m
+++ utilities/query_joints.m
@@ -1,6 +1,12 @@
 function joints = query_joints(port)
 % joints is a row vector
+if nargin == 0
+    port = udpport('byte');
+end
 writeline(port,"robot;","127.0.0.1",7755);
 s = split(readline(port), ';');
 joints = double(s(1:end-1))';
+if nargin == 0
+    clear port;
+end
 end
