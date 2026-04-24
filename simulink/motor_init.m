c52cf9366b1370513ba07468bc9302361ee3013a
wangjunchen <wangjunchen@buaa.edu.cn>
2026-04-24 18:36:51 +0800

    add my_library

--- simulink/motor_init.m
+++ simulink/motor_init.m
@@ -1,30 +1,13 @@
-% Jm = 1e-2;
-% bm = 1e-3;
-% r = 150;
-% %Ka and Kb are the same using SI unit
-% K = 0.125;
-% R = 0.5;
-% L = 0.5e-3;
-% %电流环
-% i_kp = 3.887;
-% i_ki = 3887;
-% %速度环
-% v_kp = 15;
-% v_ki = 1.8;
-% %位置环
-% p_kp = 5;
-
-
 Jm = 1e-2;
 bm = 1e-3;
-r = 100;
+r = 150;
 %Ka and Kb are the same using SI unit
-K = 0.09;
+K = 0.125;
 R = 0.5;
 L = 0.5e-3;
 %电流环
-i_kp = 1.887;
-i_ki = 1887;
+i_kp = 3.887;
+i_ki = 3887;
 %速度环
 v_kp = 15;
 v_ki = 1.8;
