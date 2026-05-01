f8100d463edbb5011b5f015741bce2f7b8143a5d
wangjunchen <wangjunchen@buaa.edu.cn>
2026-05-01 17:25:06 +0800

    update

--- simulink/render_to_MICSys.m
+++ simulink/render_to_MICSys.m
@@ -4,10 +4,10 @@ setup(block);
 end
 
 function setup(block)
+    block.NumDialogPrms = 1;
     % 输入输出端口配置
     block.NumInputPorts  = 1;
     block.NumOutputPorts = 0;
-    
     % 设置输入端口属性
     block.SetPreCompInpPortInfoToDynamic;
     block.InputPort(1).Dimensions        = -1;  % 继承维度
@@ -16,7 +16,7 @@ function setup(block)
     block.InputPort(1).Complexity        = 'Real';
     
     % 这个如果写成-1有些高频仿真会很慢！
-    block.SampleTimes = [0.05 0];
+    block.SampleTimes = [block.DialogPrm(1).Data 0];
     
     % 仿真状态兼容性
     block.SimStateCompliance = 'DefaultSimState';
--- simulink/robot_velocity_input_init.m
+++ simulink/robot_velocity_input_init.m
@@ -1,3 +1,4 @@
-rigid_body = importrobot('urdf\ur_description\urdf\ur5e-bph.urdf');
+rigid_body = importrobot('urdf\ur_description\urdf\ur5e.urdf');
 robot = get_robot_chain(rigid_body, 'tool0','world');
 init_pos = deg2rad([0, -130, -100, -40, 90, 0]);
+T_init = forward_kin_general(robot, init_pos);
